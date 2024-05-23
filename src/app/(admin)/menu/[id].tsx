5527ec410c2a6aedd7d2c92fa50a4551e8f63614
Marcus Neo <marcus121neo@gmail.com>
2024-05-23 17:15:06 +0800

    Admin Side Structure Added

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,67 @@
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
+import Button from "@/components/Button";
+import { useCart } from "@/providers/CartProvider";
+import { PizzaSize } from "@/types";
+
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const ProductDetailsScreen = () => {
+  const { id } = useLocalSearchParams();
+  const { items, addItem } = useCart();
+
+  const router = useRouter();
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+  const product = products.find((p) => p.id.toString() === id);
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  price: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
+
+export default ProductDetailsScreen;
